3850cf0fd3d75cdf530b04aced88972f8fa6cf12
Rob Barnsley <Rob Barnsley>
2018-03-02 15:12:04 +0000

    refactored the IFU_brickwall (IFU_SWIFT) logic.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ When defining a new IFU type, a new IFU object should be created that inherits a
 
 Current IFU child classes include:
 
-- "IFU_brickwall" - For a SWIFT-like IFU with demagnifying lenslets. 
+- "IFU_SWIFT" - For a SWIFT-like IFU with anamorphic preoptics and demagnifying lenslets in a brickwall pattern. 
 
 ## Caveats 
 Be aware that the entries in the configuration files do not distinguish between IFU types and thus it is possible to create an undefinable IFU. You are responsible for making sure that the IFU child class functions only access configuration items that are defined for it. For example, you may create an "IFU_brickwall" IFU but not define the `stack_to_entrance_slit_magnification` item in `slits.json`. These keyerror exceptions should be caught in the constructor of the child class.
--- ifu.py
+++ ifu.py
@@ -16,32 +16,58 @@ class IFU(object):
     if slit_config_name is not None:
       self.slit = Slit(config_dir + '/slits.json', slit_config_name)
 
-class IFU_brickwall(IFU):
+class IFU_SWIFT(IFU):
   '''
-    A SWIFT-type IFU with demagnifying lenslet array in brick-wall type pattern.
+    A SWIFT-type IFU with demagnifying lenslet array in a brick-wall type 
+    pattern.
   '''
   def __init__(self, preoptics_config_name, slicer_config_name,
     slit_config_name, config_dir="etc/configs"):
-    super(IFU_brickwall, self).__init__(preoptics_config_name, 
+    try:
+      assert preoptics_config_name is not None
+      assert slicer_config_name is not None
+      assert slit_config_name is not None
+    except AssertionError:
+      print "one of the configs has not been supplied."
+      exit(0)
+    super(IFU_SWIFT, self).__init__(preoptics_config_name, 
       slicer_config_name, slit_config_name, config_dir)
 
     try:
       self.n_slices = self.n_slitlets = self.slicer.cfg['n_slices']
+      self.preooptics_anamorphic_magnification = \
+        self.preoptics.cfg['image_scale_across_slices_arcsec_per_mm'] / \
+        self.preoptics.cfg['image_scale_along_slices_arcsec_per_mm']
+
+      self.slicer_spaxel_scale = (self.slicer.cfg['slice_width_physical'] * \
+        self.preooptics_anamorphic_magnification, \
+        self.slicer.cfg['slice_width_physical']) # (along, across)
+      
+      self.slicer_dimensions_physical = ( \
+        self.slicer_spaxel_scale[0] * self.slicer.cfg['slice_length_spaxels'], \
+        self.slicer_spaxel_scale[1] * self.n_slices) # (along, across)
+
       self.slice_active_spaxels = self.slicer.cfg['slice_length_spaxels'] - \
-        self.slicer.cfg['slice_inactive_spaxels']
-      self.slice_length_physical = self.slice_active_spaxels * \
-        self.preoptics.cfg['anamorphic_magnification'] * \
-        self.slicer.cfg['slice_width_physical']
-      self.slitlet_length_physical = self.slice_length_physical * \
+        self.slicer.cfg['slice_inactive_spaxels']  
+
+      self.slicer_dimensions_physical_active = ( \
+        self.slicer_spaxel_scale[0] * self.slice_active_spaxels, \
+        self.slicer_spaxel_scale[1] * self.n_slices) # (along, across)
+
+      self.slicer_FOV = (self.slicer_dimensions_physical_active[0] * \
+        self.preoptics.cfg['image_scale_along_slices_arcsec_per_mm']*1E3, \
+        self.slicer_dimensions_physical_active[1] * \
+        self.preoptics.cfg['image_scale_across_slices_arcsec_per_mm']*1E3)
+
+      self.slitlet_length_physical_active = \
+        self.slicer_dimensions_physical_active[0] * \
         self.slit.cfg['stack_to_entrance_slit_magnification']
+
       self.slitlet_separation_physical = \
         self.slicer.cfg['slice_inactive_spaxels'] * \
-        self.preoptics.cfg['anamorphic_magnification'] * \
-        self.slicer.cfg['slice_width_physical'] * \
+        self.slicer_spaxel_scale[0] * \
         self.slit.cfg['stack_to_entrance_slit_magnification']
-      self.slit_length_physical = (self.n_slices *  \
-        self.slitlet_length_physical) + ((self.n_slices-1) * \
-        self.slitlet_separation_physical)
+
       # following taken from geometry of packing circular lenslets
       self.stagger_length = self.slit.cfg['lenslet_diameter'] * 0.5 * (3**0.5) 
     except KeyError:
@@ -50,21 +76,29 @@ class IFU_brickwall(IFU):
         "of ifu."
       exit(0)    
 
-  def getEntranceSlitFields(self, nfields, lumultiplier=1e3, verbose=True, 
-    debug=False):
+  def getEntranceSlitFields(self, nfields, nspectrographs=2, lumultiplier=1e3, 
+    verbose=True, debug=False):
     '''
       Takes the ifu component assembly and number of fields [nfields] and 
-      returns a list of corresponding field points at the entrance slit.
+      returns a list of corresponding field points at the entrance slit. 
+
+      To get the field points, we divide the number of fields [nfields] by the 
+      number of slices, creating this many fields per slitlet. Fields will be 
+      spaced in such a way as to maximise the distance between the points. If
+      only one field per slice is requested, the field point will be at the 
+      centre. The total slit length [slit_length_physical] used for this 
+      calculation takes into account the number of spectrographs 
+      [nspectrographs] over which the slit would be divided.
 
       If interfacing with Zemax, it may be necessary to move the units to lens
       units (typically mm). This means setting the [lumultiplier] parameter.
-
-      Divides the number of fields by the number of slices, creating this 
-      many fields per slitlet. Fields will be spaced in such a way as to 
-      maximise the distance between the points. If only one field per slice 
-      is requested, the field point will be at the centre.
     '''
     fields = []
+
+    n_slices_per_spec = self.n_slices/nspectrographs
+    slit_length_physical = (n_slices_per_spec * \
+      self.slitlet_length_physical_active) + ((n_slices_per_spec-1) * \
+      self.slitlet_separation_physical)
 
     try:
       assert nfields % self.n_slitlets == 0
@@ -74,15 +108,16 @@ class IFU_brickwall(IFU):
         
     nfields_per_slitlet = int(nfields/self.n_slitlets)
     y = self.stagger_length/2.
-    for s in range(self.n_slitlets):
-      x_s = (s*(self.slitlet_length_physical + \
-        self.slitlet_separation_physical)) - self.slit_length_physical/2.
-      x_e = ((s+1)*self.slitlet_length_physical + \
-        s*self.slitlet_separation_physical) - self.slit_length_physical/2.
+    for s in range(n_slices_per_spec):
+      x_s = (s*(self.slitlet_length_physical_active + \
+        self.slitlet_separation_physical)) - slit_length_physical/2.
+      x_e = ((s+1)*self.slitlet_length_physical_active + \
+        s*self.slitlet_separation_physical) - slit_length_physical/2.
+      print x_s, x_e
       if nfields_per_slitlet == 1:
         fields.append((x_s+(x_e-x_s)/2., y))
       elif nfields_per_slitlet > 1:
-        x_sampling = self.slitlet_length_physical/(nfields_per_slitlet-1)
+        x_sampling = self.slitlet_length_physical_active/(nfields_per_slitlet-1)
         for x in range(0, nfields_per_slitlet):
           fields.append((x_s + (x * x_sampling), y))
       y = -y
